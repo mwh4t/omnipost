c721f0fdb1d3f1df59ecd2289d64390fb64163f4
mwh4t <korzunov.matvey@yandex.ru>
2025-11-01 12:14:21 +0300

    first commit

---
+++ omnipost/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for omnipost project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omnipost.settings')
+
+application = get_asgi_application()
